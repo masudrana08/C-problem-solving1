500609a967fc55201b2d6b9fc94af111b2ea3174
masudrana08 <rana028511@gmail.com>
2022-09-20 13:19:39 +0600

    'salary'

---
+++ 8salary/salary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+   int employee, workHour;
+   float rate;
+   scanf("%d%d%f", &employee, &workHour, &rate);
+   printf("NUMBER = %d\n", employee);
+   printf("SALARY = U$ %.2f\n", (float)workHour*rate);
+   return 0;
+}
