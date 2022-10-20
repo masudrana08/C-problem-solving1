2c22fcc5880a89503c569b2b8e6f75e8b5934cbd
masudrana08 <rana028511@gmail.com>
2022-10-20 21:05:01 +0600

    practice day 1

---
+++ practice/day1.c
@@ -0,0 +1,58 @@
+// Simple output
+// #include <stdio.h>
+
+// int main(){
+//    printf("Hi there /\\\t I hope you are doing great and practicing well.\nNow this is a simple task you need to do.\tJust care for the indentation.\nShow 100%% sincerity");
+//    return 0;
+// }
+
+// Print value
+// #include <stdio.h>
+
+// int main(){
+//    int x = 10;
+//    int y = 20;
+//    printf("%d %d", y, x);
+//    return 0;
+// }
+
+// Multiplied by two
+// #include <stdio.h>
+
+// int main(){
+//    int x;
+//    scanf("%d",&x);
+//    printf("%d", x*2);
+//    return 0;
+// }
+
+// Summation
+// #include <stdio.h>
+
+// int main(){
+//    int x,y;
+//    scanf("%d%d",&x,&y);
+//    printf("%d", x+y);
+//    return 0;
+// }
+
+
+// Character to ASCII
+// #include <stdio.h>
+
+// int main(){
+//    char x;
+//    scanf("%c",&x);
+//    printf("%d", x);
+//    return 0;
+// }
+
+// % value input
+#include <stdio.h>
+
+int main(){
+   int x;
+   scanf("%d",&x);
+   printf("%d", x);
+   return 0;
+}
