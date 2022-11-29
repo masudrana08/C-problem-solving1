6580a12d0fb5853041e42fd2c3b28ed4e46316f8
masudrana08 <rana028511@gmail.com>
2022-11-29 15:30:52 +0600

    assignment done

--- assignment/theory.c
+++ assignment/theory.c
@@ -224,3 +224,17 @@
    
 //    return 0;
 // }
+
+
+// #include<stdio.h>
+// int value=3;
+// void foo(int n)
+// {
+//     value*=2;
+// }
+// int main()
+// {
+//     printf("%d \n",value);
+//     foo(2);
+//     return 0;
+// }
